e48a014acafb7c8beeb85da0c8f77205f7402a2b
Evan Lloyd New-Schmidt <evan@new-schmidt.com>
2020-04-13 20:00:26 -0400

    It works!

---
+++ README.md
@@ -0,0 +1,7 @@
+# aframe-copresence
+
+A (minimal, hacky) way to share user positions across `A-Frame` instances, using `socket.io`.
+
+Run `node index.js` to start the server, visit it from multiple browsers to see it work!
+
+![](docs/demo.gif)
---
+++ index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('update-pos', (pos) => {
+        // console.log(pos);
+        socket.broadcast.emit('visitor-update-pos', {id: socket.id, ...pos});
+    })
+    socket.on('disconnect', function() {
+        socket.broadcast.emit('visitor-disconnect', {id: socket.id});
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
