6f7fbfe62646a8ee52596fa12abfcc0501c34b68
Evan Lloyd New-Schmidt <evan@new-schmidt.com>
2020-04-14 11:40:56 -0400

    Make position updates less reliable, and hopefully less laggy

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ io.on('connection', function(socket){
     console.log('a user connected');
     socket.on('update-pos', (pos) => {
         // console.log(pos);
-        socket.broadcast.emit('visitor-update-pos', {id: socket.id, ...pos});
+        socket.volatile.broadcast.emit('visitor-update-pos', {id: socket.id, ...pos});
     })
     socket.on('disconnect', function() {
         socket.broadcast.emit('visitor-disconnect', {id: socket.id});
