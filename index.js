7f3efb09f4c5b9619041de02ed5fe54c53eb83a0
Evan Lloyd New-Schmidt <evan@new-schmidt.com>
2020-04-30 13:13:53 -0400

    Set port with env

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
@@ -18,6 +20,6 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(PORT, function(){
+    console.log(`listening on *:${PORT}`);
 });
