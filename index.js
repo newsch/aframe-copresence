9e82c48a32ed323fbc473d799264b0eb7105db6d
Evan Lloyd New-Schmidt <evan@new-schmidt.com>
2020-05-16 15:26:54 -0400

    Send current clients on connect

--- index.js
+++ index.js
@@ -12,18 +12,35 @@ app.get('/client.js', function(req, res) {
     res.sendFile(__dirname + '/client.js');
 });
 
+var clients = new Map();
+
 io.on('connection', function(socket){
     console.log(`visitor connected: ${socket.id} (${socket.handshake.address})`);
+    clients.forEach((client, id) => {
+        socket.emit('visitor-update-pos', {id, position: client.position, rotation: client.rotation});
+        socket.emit('visitor-update-data', {id, data: client.data});
+    });
+    socket.emit('visitor-list', clients);
+    // Now add self to clients
+    let me = {id: socket.id, position: {x: 0, y: 0, z: 0}, rotation: {x: 0, y: 0, z: 0}, data: {}};
+    clients.set(socket.id, me);
+
     socket.on('update-pos', (pos) => {
         // console.log(pos);
         socket.volatile.broadcast.emit('visitor-update-pos', {id: socket.id, ...pos});
+        let client = clients.get(socket.id);
+        client.position = pos.position;
+        client.rotation = pos.rotation;
     })
     socket.on('update-data', (data) => {
         socket.broadcast.emit('visitor-update-data', {id: socket.id, data});
+        let client = clients.get(socket.id);
+        client.data = {...client.data, ...data};
     });
     socket.on('disconnect', function() {
         socket.broadcast.emit('visitor-disconnect', {id: socket.id});
         console.log(`visitor disconnected: ${socket.id} (${socket.handshake.address})`);
+        clients.delete(socket.id);
     });
 });
 
